dbb2c1ffe030ce56c9d31540be370f9f5f54087a
cluper-LEE <wls4370@gmail.com>
2021-07-27 16:00:20 +0900

    Set models/index.js

--- models/index.js
+++ models/index.js
@@ -1,37 +1,38 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+
+/*
+모델 임포트
+const User = require('./user');
+const Member = require('./member');
+const Team = require('./team');
+const Project = require('./project');
+*/
+
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
+/*
+db.User = User;
+db.Member = Member;
+db.Team = Team;
+db.Project = Project;
+
+모델 생성
+User.init(sequelize);
+Member.init(sequelize);
+Team.init(sequelize);
+Project.init(sequelize);
+
+모델 관계 설정
+User.associate(db);
+Member.associate(db);
+Team.associate(db);
+Project.associate(db);
+*/
 
 module.exports = db;
