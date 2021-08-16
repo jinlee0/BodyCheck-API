82029efbef574a22604dbce89c3fc5ae8330f97b
HyunSBong <sanseng@mju.ac.kr>
2021-08-16 10:15:29 +0900

    reset dateRecords

--- routes/dateRecords.js
+++ routes/dateRecords.js
@@ -5,25 +5,23 @@ const { File, DateRecord, Record } = require("../models");
 const router = express.Router();
 
 // Read
-// query (date)
-router.get("/",
-  isLoggedIn, 
-  async (req, res, next) => {
+router.get("/", isLoggedIn, async (req, res, next) => {
   try {
     // query options: date
     const {date} = req.query;
     
+    let where = {};
+    if(date !== undefined){
+      where.date = date;
+    }
     if(date === ''){
-      return res.status(400).json(getFailure(`does not found ${date}`));
+      return res.status(400).json(getFailure(req.originalUrl + ' date is not nullable'));
     }
 
-    const dateRecords = await DateRecord.findAll({
-      where: { date: date }
-    });
-    if (!dateRecords || dateRecords.length == 0) {
-      return res.status(500).json(getFailure(`db error: read via ${date}`));
+    const dateRecords = await DateRecord.findAll({where});
+    if(dateRecords.length === 0){
+      return res.status(204).json();
     }
-
     return res.status(200).json(getSuccess(dateRecords));
   } catch (error) {
     console.error(error);
@@ -31,19 +29,13 @@ router.get("/",
   }
 });
 
-// read
-// params (id)
-router.get('/:id', 
-  isLoggedIn, 
-  async (req, res, next) => {
+router.get('/:id', isLoggedIn, async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const {id} = req.params;
     
-    const dateRecord = await DateRecord.findOne({
-      where: { id }
-    });
+    const dateRecord = await DateRecord.findOne({where:{id}});
     if(!dateRecord){
-      return res.status(404).json(`does not found dateRecord via id`);
+      return res.status(404).json(getFailure(req.originalUrl + ' id'));
     }
 
     return res.status(200).json(getSuccess(dateRecord));
@@ -54,43 +46,36 @@ router.get('/:id',
 })
 
 // Create
-router.post("/", 
-  isLoggedIn, 
-  async (req, res, next) => {
-  const {date,startTime,endTime,memo} = req.body;
-  const record = await DateRecord.create({
-    date,
-    startTime,
-    endTime,
-    memo,
-  });
-  if(!record){
-    return res.status(500).json(getFailure(`db error: create`));
-  }
-  
-  return res.status(201).json(getSuccess(record));
+router.post("/", isLoggedIn, async (req, res, next) => {
+        const {date,startTime,endTime,memo} = req.body;
+        const record = await DateRecord.create({
+          date,
+          startTime,
+          endTime,
+          memo,
+        });
+        if(!record){
+          return res.status(500).json(getFailure(`db error: create`));
+        }
+        return res.status(201).json(getSuccess(record));
 });
 
 // Update
-router.patch("/:id", 
-  isLoggedIn, 
-  async (req, res, next) => {
+router.patch("/:id", isLoggedIn, async (req, res, next) => {
   try {
     const {date,startTime,endTime,memo} = req.body;
 
     if(date===undefined && startTime===undefined && endTime===undefined && memo===undefined){
-      return res.status(400).json(getFailure(`Some of the data has not been sent.`));
+      return res.status(400).json(getFailure(req.originalUrl+ ' At least one content required'));
     }
 
     if(date===null){
-      return res.status(400).json(getFailure(`does not found date`));
+      return res.status(400).json(getFailure(req.originalUrl + ' Date is not nullable'));
     }
 
-    const dateRecord = await DateRecord.findOne({
-      where: {id: req.params.id}
-    });
+    const dateRecord = await DateRecord.findOne({where: {id: req.params.id}});
     if(!dateRecord){
-      return res.status(404).json(getFailure(`does not found dateRecord via id`));
+      return res.status(404).json(getFailure(req.originalUrl + ' id'));
     }
 
     const isSame = await updateForEach(dateRecord, {date, startTime, endTime, memo});
@@ -107,21 +92,16 @@ router.patch("/:id",
 });
 
 // Delete
-router.delete("/:id", 
-  isLoggedIn, 
-  async (req, res, next) => {
+router.delete("/:id", isLoggedIn, async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    let dateRecord = await DateRecord.findOne({where:{id}});
+    const dateRecord = await DateRecord.findOne({where:{id}});
     if(!dateRecord){
-      return res.status(404).json(`does not found dateRecord via id`);
+      return res.status(404).json(req.originalUrl + ' id');
     }
-    dateRecord = await dateRecord.destroy();
-    if (!dateRecord) {
-      return res.status(500).json(`db error: destroy`)
-    }
-    return res.status(204).json(dateRecord);
+    await dateRecord.destroy();
+    return res.status(204).json();
   } catch (error) {
     console.error(error);
     next(error);
