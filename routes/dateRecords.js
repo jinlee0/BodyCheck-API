94e8a48f9177ca65df8c550fa69e2389f2bd7056
cluper-LEE <wls4370@gmail.com>
2021-08-28 12:27:33 +0900

    Add code about relationship of DateRecord and User

--- routes/dateRecords.js
+++ routes/dateRecords.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const multer = require("multer");
 const { isLoggedIn, getSuccess, getFailure, updateForEach } = require("./middlewares");
-const { File, DateRecord, Record } = require("../models");
+const { File, DateRecord, Record, User } = require("../models");
 const router = express.Router();
 
 // Read
 router.get("/", isLoggedIn, async (req, res, next) => {
   try {
     // query options: date
-    const {date} = req.query;
+    const {date, UserId} = req.query;
     
     let where = {};
-    if(date !== undefined){
+
+    if(date){
+      if(date === ''){
+        return res.status(400).json(getFailure(req.originalUrl + ' date cant be blank'));
+      }
       where.date = date;
     }
-    if(date === ''){
-      return res.status(400).json(getFailure(req.originalUrl + ' date is not nullable'));
+    if(UserId){
+      const user = await User.findByPk(UserId);
+      if(!user){
+        return res.status(404).json(getFailure(req.originalUrl + ' UserId'));
+      }
+      where.UserId = UserId;
     }
 
     const dateRecords = await DateRecord.findAll({where});
@@ -33,7 +41,7 @@ router.get('/:id', isLoggedIn, async (req, res, next) => {
   try {
     const {id} = req.params;
     
-    const dateRecord = await DateRecord.findOne({where:{id}});
+    const dateRecord = await DateRecord.findByPk(id);
     if(!dateRecord){
       return res.status(404).json(getFailure(req.originalUrl + ' id'));
     }
@@ -48,7 +56,12 @@ router.get('/:id', isLoggedIn, async (req, res, next) => {
 // Create
 router.post("/", isLoggedIn, async (req, res, next) => {
   try{
-    const {date,startTime,endTime,memo} = req.body;
+    const {date,startTime,endTime,memo,UserId} = req.body;
+
+    const user = await User.findByPk(UserId);
+    if(!user){
+      return res.status(404).json(getFailure(req.originalUrl + ' UserId'));
+    }
 
     if(!date){
       return res.status(400).json(getFailure(req.originalUrl+'Date is not nullable'));
@@ -59,6 +72,7 @@ router.post("/", isLoggedIn, async (req, res, next) => {
       startTime,
       endTime,
       memo,
+      UserId,
     });
     if(!record){
       return res.status(500).json(getFailure(`db error: create`));
@@ -73,22 +87,23 @@ router.post("/", isLoggedIn, async (req, res, next) => {
 // Update
 router.patch("/:id", isLoggedIn, async (req, res, next) => {
   try {
-    const {date,startTime,endTime,memo} = req.body;
+    const {date,startTime,endTime,memo,UserId} = req.body;
+    const {id} = req.params;
 
-    if(date===undefined && startTime===undefined && endTime===undefined && memo===undefined){
+    if(date===undefined && startTime===undefined && endTime===undefined && memo===undefined && UserId===undefined){
       return res.status(400).json(getFailure(req.originalUrl+ ' At least one content required'));
     }
 
-    if(date===null){
-      return res.status(400).json(getFailure(req.originalUrl + ' Date is not nullable'));
+    if(date===null || UserId === null){
+      return res.status(400).json(getFailure(req.originalUrl + ' Date and UserId are not nullable'));
     }
 
-    const dateRecord = await DateRecord.findOne({where: {id: req.params.id}});
+    const dateRecord = await DateRecord.findByPk(id);
     if(!dateRecord){
       return res.status(404).json(getFailure(req.originalUrl + ' id'));
     }
 
-    const isSame = await updateForEach(dateRecord, {date, startTime, endTime, memo});
+    const isSame = await updateForEach(dateRecord, {date, startTime, endTime, memo, UserId});
     if(isSame){
       return res.status(204).json();
     }
@@ -106,7 +121,7 @@ router.delete("/:id", isLoggedIn, async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const dateRecord = await DateRecord.findOne({where:{id}});
+    const dateRecord = await DateRecord.findByPk(id);
     if(!dateRecord){
       return res.status(404).json(req.originalUrl + ' id');
     }
